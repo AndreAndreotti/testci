278d0c4d97f4f1d242ed38b2ed74350d0ec2b970
Renan Pupin <renan.pupin@gmail.com>
2018-08-31 18:09:56 -0300

    initial commit

---
+++ test/basic.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const expect = chai.expect;
+const should = chai.should();
+
+describe('Test that tests run', function(done) {
+  it('should run a test', function(done) {
+    expect(true).to.eql(true);
+    done();
+  });
+});
