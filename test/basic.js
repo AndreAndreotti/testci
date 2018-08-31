7036abb36a4881944b9b562ef0751e91fa3ebbf9
Andreotti <dias.andreotti@gmail.com>
2018-08-31 18:18:58 -0300

    test

--- test/basic.js
+++ test/basic.js
@@ -4,7 +4,7 @@ const should = chai.should();
 
 describe('Test that tests run', function(done) {
   it('should run a test', function(done) {
-    expect(true).to.eql(true);
+    expect(true).to.eql(false);
     done();
   });
 });
